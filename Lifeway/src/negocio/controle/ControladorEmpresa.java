b45967d40de24d91a98a6d426bd594c73cb5bb2a
Ronaldo Rodrigues <74150316+Rona1f@users.noreply.github.com>
2021-06-22 23:25:40 -0300

    Update ControladorEmpresa.java

--- Lifeway/src/negocio/controle/ControladorEmpresa.java
+++ Lifeway/src/negocio/controle/ControladorEmpresa.java
@@ -1,5 +1,40 @@
 package negocio.controle;
 
+import java.util.List;
+
+import dados.IRepositorioGenerico;
+import dados.RepositorioGenerico;
+import excecoes.ObjetoJaExisteException;
+import negocio.beans.Empresa;
+
 public class ControladorEmpresa {
     
+
+    private static ControladorEmpresa instance;
+
+    private IRepositorioGenerico<Empresa> repositorioEmpresa;
+
+    ControladorEmpresa() {
+        this.repositorioEmpresa = new RepositorioGenerico<>();
+    }
+
+    public ControladorEmpresa getInstance() {
+        if(instance == null) {
+            instance = new ControladorEmpresa();
+        }
+        return instance;
+    }
+
+    public void criarNovaEmpresa(Empresa empresa) throws ObjetoJaExisteException {
+        repositorioEmpresa.inserir(empresa);
+    }
+
+    public void excluirEmpresa(Empresa empresa) {
+        repositorioEmpresa.remover(empresa);
+    }
+
+    public List<Empresa> listarEmpresas() {
+        return repositorioEmpresa.listar();
+    }
+
 }
