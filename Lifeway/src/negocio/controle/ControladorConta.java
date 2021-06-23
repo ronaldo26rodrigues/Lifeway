75bf7475aab9a00ca8a8bd2e6b8a2d70ca3b0772
Gabriel Santos <gabrielsantosslv@outlook.com>
2021-06-22 23:20:17 -0300

    Controlador Contaaas

--- Lifeway/src/negocio/controle/ControladorConta.java
+++ Lifeway/src/negocio/controle/ControladorConta.java
@@ -1,5 +1,39 @@
 package negocio.controle;
 
+import java.util.List;
+
+import dados.IRepositorioGenerico;
+import dados.RepositorioGenerico;
+import excecoes.ObjetoJaExisteException;
+import negocio.beans.Conta;
+
 public class ControladorConta {
+
+    private static ControladorConta instance;
     
+    private IRepositorioGenerico<Conta> repositorioContas;
+
+    
+    ControladorConta() {
+        this.repositorioContas = new RepositorioGenerico<>();
+    }
+
+    public ControladorConta getInstance() {
+        if(instance == null){
+            instance = new ControladorConta();
+        }
+        return instance;
+    }
+
+    public void criarNovaConta(Conta conta) throws ObjetoJaExisteException {
+        repositorioContas.inserir(conta);
+    }
+
+    public void excluirConta(Conta conta) throws ObjetoJaExisteException {
+        repositorioContas.remover(conta);
+    }
+
+    public List<Conta> listarContas() {
+        return repositorioContas.listar();
+    }
 }
