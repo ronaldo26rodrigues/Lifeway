899adab593ef609567730c51cedf2190cca9bd9c
Ronaldo Rodrigues <74150316+Rona1f@users.noreply.github.com>
2021-06-23 17:34:49 -0300

    Update ControladorRO.java

--- Lifeway/src/negocio/controle/ControladorRO.java
+++ Lifeway/src/negocio/controle/ControladorRO.java
@@ -1,9 +1,14 @@
 package negocio.controle;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 import dados.IRepositorioGenerico;
 import dados.RepositorioGenerico;
-import jdk.vm.ci.code.RegisterAttributes;
+import negocio.beans.Empresa;
+import negocio.beans.Endereco;
 import negocio.beans.RegistroDeOcorrencia;
+import negocio.beans.Usuario;
 
 public class ControladorRO {
 
@@ -23,8 +28,27 @@ public class ControladorRO {
         return instance;
     }
 
-    public void criarNovaOcorrencia() {
+    public void criarNovaOcorrencia(String assunto, String mensagem, Empresa empresa, Usuario cliente, LocalDate data, Endereco endereco) {
         
+        Random rng = new Random();
+
+        RegistroDeOcorrencia novaOcorrencia = new RegistroDeOcorrencia("P"+rng.nextInt(1000)+rng.nextInt(1000), assunto, mensagem, empresa, cliente, data, endereco);
+        try {
+            repositorioRO.inserir(novaOcorrencia);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+        
+    }
+
+    public void resolverOcorrencia(RegistroDeOcorrencia ocorrencia) {
+        for (RegistroDeOcorrencia registroDeOcorrencia : repositorioRO.listar()) {
+            if(ocorrencia.equals(registroDeOcorrencia)) {
+                registroDeOcorrencia.setResolvido(true);
+                break;
+            }
+        }
     }
 
 }
