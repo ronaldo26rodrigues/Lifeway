5718df4f12d486b335a399ae174d369bf4b3593b
Gabriel Santos <gabrielsantosslv@outlook.com>
2021-06-22 22:26:39 -0300

    imports controlador

--- Lifeway/src/negocio/controle/Controlador.java
+++ Lifeway/src/negocio/controle/Controlador.java
@@ -1,5 +1,10 @@
 package negocio.controle;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 public class Controlador {
     
 }
